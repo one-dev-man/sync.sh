7986b3c06a7e9f26bcc8ed338f96feadb104ff06
OneDevMan <enderfight95@gmail.com>
2023-04-26 14:31:47 +0200

    update

--- setup/autocompletion.sh
+++ setup/autocompletion.sh
@@ -13,19 +13,19 @@ autocomplete () {
 
     local tasklist=("init" "sync" "remote" "shell")
 
-    local p=$(eval echo $CURRENT_CONFIGFILE)
-    if [ $(find "$p" > /dev/null 2>&1 ; echo $?) -eq 0 ]; then
-        local current_config_content=$(eval printf '"''$(<"'$p'")''"')
-        local raw_tasklist="$(printf "$current_config_content" | grep ^TASK_)"
+    # local p=$(eval echo $CURRENT_CONFIGFILE)
+    # if [ $(find "$p" > /dev/null 2>&1 ; echo $?) -eq 0 ]; then
+    #     local current_config_content=$(eval printf '"''$(<"'$p'")''"')
+    #     local raw_tasklist="$(printf "$current_config_content" | grep ^TASK_)"
 
-        for raw_task in $raw_tasklist; do
-            local task=${raw_task/=*/}
-            task=${task/*TASK_/}
-            task=${task,,}
+    #     for raw_task in $raw_tasklist; do
+    #         local task=${raw_task/=*/}
+    #         task=${task/*TASK_/}
+    #         task=${task,,}
             
-            tasklist[${#tasklist[@]}]="$task"
-        done
-    fi
+    #         tasklist[${#tasklist[@]}]="$task"
+    #     done
+    # fi
 
     # 
 
